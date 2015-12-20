623afd34d4e94b1786cdca3cef62a5e6bc0d86a9
Bojan Delic <bojan@delic.in.rs>
2015-12-20 22:10:10 +0100

    Do not repeate logging logic in default.

--- default.go
+++ default.go
@@ -10,80 +10,65 @@ package ligno
 // will be translated into log record with following keys:
 //  {LEVEL: INFO", EVENT: "User logged in", "user_id": user_id, "platform": PLATFORM_NAME}
 func Log(level Level, event string, pairs ...string) {
-	var d = make(Record)
-	d[EventKey] = event
-	d[LevelKey] = level
-	// make sure that number of items in data is even
-	if len(pairs)%2 != 0 {
-		pairs = append(pairs, "")
-	}
-	for i := 0; i < len(pairs); i += 2 {
-		d[pairs[i]] = pairs[i+1]
-	}
-	defaultLog.log(d)
+	defaultLog.Log(level, event, pairs...)
 }
 
 // LogRecord adds provided record to queue for processing.
 func LogRecord(record Record) {
-	defaultLog.log(record)
+	defaultLog.LogRecord(record)
 }
 
 // Debug creates log record and queues it for processing with DEBUG level.
 // Additional parameters have same semantics as in Log method.
 func Debug(event string, pairs ...string) {
-	defaultLog.Log(DEBUG, event, pairs...)
+	defaultLog.Debug(event, pairs...)
 }
 
 // DebugRecord adds DEBUG level to provided record and queues it for processing.
 func DebugRecord(record Record) {
-	record[LevelKey] = DEBUG
-	defaultLog.LogRecord(record)
+	defaultLog.DebugRecord(record)
 }
 
 // Info creates log record and queues it for processing with INFO level.
 // Additional parameters have same semantics as in Log method.
 func Info(event string, pairs ...string) {
-	defaultLog.Log(INFO, event, pairs...)
+	defaultLog.Info(event, pairs...)
 }
 
 // InfoRecord adds INFO level to provided record and queues it for processing.
 func InfoRecord(record Record) {
-	record[LevelKey] = INFO
-	defaultLog.LogRecord(record)
+	defaultLog.InfoRecord(record)
 }
 
 // Warning creates log record and queues it for processing with WARNING level.
 // Additional parameters have same semantics as in Log method.
 func Warning(event string, pairs ...string) {
-	defaultLog.Log(WARNING, event, pairs...)
+	defaultLog.Warning(event, pairs...)
 }
 
 // WarningRecord adds WARNING level to provided record and queues it for processing.
 func WarningRecord(record Record) {
-	record[LevelKey] = WARNING
-	defaultLog.LogRecord(record)
+	defaultLog.WarningRecord(record)
 }
 
 // Error creates log record and queues it for processing with ERROR level.
 // Additional parameters have same semantics as in Log method.
 func Error(event string, pairs ...string) {
-	defaultLog.Log(ERROR, event, pairs...)
+	defaultLog.Error(event, pairs...)
 }
 
 // ErrorRecord adds ERROR level to provided record and queues it for processing.
 func ErrorRecord(record Record) {
-	record[LevelKey] = ERROR
-	defaultLog.LogRecord(record)
+	defaultLog.ErrorRecord(record)
 }
 
 // Critical creates log record and queues it for processing with CRITICAL level.
 // Additional parameters have same semantics as in Log method.
 func Critical(event string, pairs ...string) {
-	defaultLog.Log(CRITICAL, event, pairs...)
+	defaultLog.Critical(event, pairs...)
 }
 
 // CriticalRecord adds CRITICAL level to provided record and queues it for processing.
 func CriticalRecord(record Record) {
-	record[LevelKey] = INFO
-	defaultLog.LogRecord(record)
+	defaultLog.CriticalRecord(record)
 }
